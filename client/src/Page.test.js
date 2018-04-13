dd442792036d7028359e306a56f878a658e5f27b
Anirudh Iyer <lopaneyo@gmail.com>
2018-04-13 08:49:25 -0400

    Fixed some compile issues

--- client/src/Page.test.js
+++ client/src/Page.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Page from './Page';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
